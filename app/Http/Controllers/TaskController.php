e7092587498cfd16f0778ae22e67e4661c6265ba
dnatha <rickygx.90@gmail.com>
2024-03-01 19:37:30 -0500

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+        return $task;
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(['message' => 'Task deleted']);
+    }
+}
